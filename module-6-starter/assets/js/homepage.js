20a03b2a9509adf0b2388d0148d705288acf2cf5
tparlmer <tparlmer@gmail.com>
2022-03-10 08:08:58 -0500

    added error handling

--- module-6-starter/assets/js/homepage.js
+++ module-6-starter/assets/js/homepage.js
@@ -1,3 +1,9 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+
 var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -5,10 +11,72 @@ var getUserRepos = function(user) {
     // make a request to the url
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) { //.json() method formats the response data as a json file
-            console.log(data);
+            displayRepos(data, user);
         });
     });
 
 };
 
-getUserRepos();
+var formSubmitHandler = function(event) {
+    event.preventDefault(); // this stops the browserfrom performing the default action the event wants it to do. In teh case of submitting a form, it prevents the browser froms endign the form's input data to a URL, as we'll handle what happens with the form input data ourselves in JavaScript
+    
+    // get value from input element
+    var username = nameInputEl.value.trim();
+
+    // check if a value has been placed in the username variable
+    if (username) {
+        // the below line of code passes the username to the getUserRepos function
+        getUserRepos(username);
+        // the below line of code clears the input value and returns it to a blank string
+        nameInputEl.value = "";
+    } else {
+        alert("Please enter a GitHub username");
+    }
+};
+
+var displayRepos = function(repos, searchTerm) {
+    console.log(repos);
+    console.log(searchTerm);
+
+    // clear old content
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+
+    // loop over repos
+    // very important to understand how this generates content for a webpage
+    for (var i = 0; i < repos.length; i++) {
+        // format repo name
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+        // create a container for each repo
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+        // create a span element to hold repository name
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        // append to container
+        repoEl.appendChild(titleEl);
+
+        // create a status element
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        // check if current repo has issues or not
+        if (repos[i].open_issues_count > 0) {
+            statusEl.innerHTML = 
+                "<i class = 'fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+        } else {
+            statusEl.innerHTML = "<i class = 'fas fa-check-square status-icon icon-success'></i>";
+        }
+
+        // append to container
+        repoEl.appendChild(statusEl);
+
+        // append container to the DOM
+        repoContainerEl.appendChild(repoEl);
+    }
+};
+
+userFormEl.addEventListener("submit", formSubmitHandler);
